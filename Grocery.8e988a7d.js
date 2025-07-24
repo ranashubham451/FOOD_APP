014d2c1168743f4c7024736d8e7c06a26586fd73
Shubham Rana <shubhamrana53.in@gmail.com>
2025-07-24 22:38:57 +0530

    Updates

---
+++ Grocery.8e988a7d.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequiref53a;(0,e.register)("loR3J",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>t,set:void 0,enumerable:!0,configurable:!0});var i=e("ayMG0"),t=()=>(0,i.jsx)("h1",{children:"Our Grocerry Online  Store ,and we have a lot of child component inside this "})});
+//# sourceMappingURL=Grocery.8e988a7d.js.map
